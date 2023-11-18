be01b13da305f012672bab321170f5ed37b0325d
kkkkkkkkazuya <kkazuya.baseball0508@gmail.com>
2023-11-18 13:24:48 +0900

    Create Block constructor and fmt logs

--- main.go
+++ main.go
@@ -3,13 +3,43 @@ package main
 import (
 	"fmt"
 	"log"
+	"time"
 )
+
+/**
+* Blcok struct
+**/
+type Block struct {
+	nonce        int
+	previousHash string
+	timestamp    int64
+	transactions []string
+}
+
+/**
+* Blcok constructor
+**/
+func NewBlock(nonce int, previousHash string) *Block {
+	b := new(Block)
+	b.timestamp = time.Now().UnixNano()
+	b.nonce = nonce
+	b.previousHash = previousHash
+	return b
+}
+
+func (b *Block) Print() {
+	fmt.Printf("timestamp:        %d\n", b.timestamp)
+	fmt.Printf("nonce:            %d\n", b.nonce)
+	fmt.Printf("previousHash:     %s\n", b.previousHash)
+	fmt.Printf("transactions:     %s\n", b.transactions)
+
+}
 
 func init() {
 	log.SetPrefix("Blockchain: ")
 }
 
 func main() {
-	log.Print("test")
-	fmt.Println("test2")
+	b := NewBlock(0, "init hash")
+	b.Print()
 }
